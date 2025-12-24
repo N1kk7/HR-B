6580f329da4a93395a7d3c2e92b9ff7a282e6c1e
Nikk7 <nick7.dev@gmail.com>
2025-12-24 14:45:46 +0200

    test

--- app/app.vue
+++ app/app.vue
@@ -10,6 +10,34 @@
 
 import { onMounted } from 'vue'
 
+useHead({
+  link: [
+    {
+      rel: 'preload',
+      as: 'font',
+      href: './assets/fonts/Roboto-Light.woff2',
+      type: 'font/woff2',
+      crossorigin: 'anonymous'
+    },
+    {
+      rel: 'preload',
+      as: 'font',
+      href: './assets/fonts/Roboto-Regular.woff2',
+      type: 'font/woff2',
+      crossorigin: 'anonymous'
+    }
+  ],
+   style: [
+    {
+      children: `
+        .hero { width: 100%; height: 600px; }
+        .hero_img { transform: scale(1.15); opacity: 0.7; }
+        .header_content { display: flex; justify-content: space-between; }
+      `
+    }
+  ]
+})
+
 onMounted(async () => {
   await import('@/assets/style/main.scss')
 })
